945ef6d5400d086e4c1879b51c29d9132fc9f452
Prashant K Bhardawaj <96685622+prashantreact@users.noreply.github.com>
2024-03-19 11:03:09 +0530

    lengthOfLongestSubstring

---
+++ Day-49/lengthOfLongestSubstring.js
@@ -0,0 +1,47 @@
+// Day-49
+/*Question:-
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+/*--- Solution--- */
+
+function lengthOfLongestSubstring(s) {
+  let maxLength = 0;
+  let start = 0;
+  const charSet = new Set(); 
+  
+  for (let end = 0; end < s.length; end++) {
+      const currentChar = s[end];
+      
+      while (charSet.has(currentChar)) {
+          charSet.delete(s[start]);
+          start++;
+      }
+      
+      charSet.add(currentChar);
+      
+      maxLength = Math.max(maxLength, end - start + 1);
+  }
+  
+  return maxLength;
+}
+
+// Example :
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
