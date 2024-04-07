4c17895e9e832d16778d2f8eb243a144e733ba17
Prashant K Bhardawaj <96685622+prashantreact@users.noreply.github.com>
2024-04-08 00:14:47 +0530

    countMatches

---
+++ Day-72/countMatches.js
@@ -0,0 +1,47 @@
+// Day-72
+/*Question:-
+
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+Example 1:
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+Example 2:
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+*/
+
+/*--- Solution--- */
+
+const countMatches=(items, ruleKey, ruleValue) =>{
+  let count = 0;
+  
+  for (const item of items) {
+      let index;
+      if (ruleKey === 'type') {
+          index = 0;
+      } else if (ruleKey === 'color') {
+          index = 1;
+      } else if (ruleKey === 'name') {
+          index = 2;
+      }
+      if (item[index] === ruleValue) {
+          count++;
+      }
+  }
+  
+  return count;
+}
+
+// Example:- 
+console.log(countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "color", "silver")); // Output: 1
+console.log(countMatches([["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], "type", "phone")); // Output: 2
