89cb653151a7c090ced9dad44c87e8be101d9ab1
Prashant K Bhardawaj <96685622+prashantreact@users.noreply.github.com>
2024-04-20 11:22:37 +0530

    rotate the array

---
+++ Days-81/arrayRotate.js
@@ -0,0 +1,55 @@
+// Day-81
+/*Question:-
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+Constraints:
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+*/
+
+/*--- Solution--- */
+const rotate = (nums, k) => {
+  const reverse = (arr, start, end) => {
+    while (start < end) {
+      let temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+  };
+
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+};
+
+// Example:
+let nums1 = [1, 2, 3, 4, 5, 6, 7];
+let k1 = 3;
+rotate(nums1, k1);
+console.log(nums1); // Output: [5, 6, 7, 1, 2, 3, 4]
+
+let nums2 = [-1, -100, 3, 99];
+let k2 = 2;
+rotate(nums2, k2);
+console.log(nums2); // Output: [3, 99, -1, -100]
