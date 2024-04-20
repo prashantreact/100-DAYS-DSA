b03008636d3e3cd383759901d6ba85d2608bf253
Prashant Bhardwaj <prashant.k@etelligens.in>
2024-04-20 11:37:52 +0530

    arrayRotate

--- Day-81/arrayRotate.js
+++ Day-81/arrayRotate.js
@@ -3,7 +3,6 @@
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
 Example 1:
-
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
 Explanation:
@@ -12,7 +11,6 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
 Example 2:
-
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
 Explanation: 
